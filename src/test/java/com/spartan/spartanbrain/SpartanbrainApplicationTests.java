4d5fefc24280e7e789ee3e491e87caec66c0b879
Omar Cervantes <omarcervantes_vega@hotmail.com>
2021-12-28 10:16:18 -0600

    28-12-2021 10:16 OOCV

---
+++ src/test/java/com/spartan/spartanbrain/SpartanbrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartan.spartanbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartanbrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
